963dd9cb045ed6bb4b760708c20fa5eafe7a131d
Student <student@d3c120f6a6.(none)>
2015-02-26 15:05:28 -0800

    Made changes.

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -5,8 +5,8 @@ public class HelloGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello, Git!");
+		System.out.println("My name is Ronald.");
 	}
 
 }
