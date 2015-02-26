7ec5cda2d71984392045cbf326aade4e0f6a9801
Student <student@d3c120f6a6.(none)>
2015-02-26 15:04:39 -0800

    Made changes.

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -7,6 +7,7 @@ public class SecondClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testing...");
+		System.out.println("Another test...");
 	}
 
 }
